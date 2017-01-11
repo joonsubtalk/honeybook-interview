0b76ce78fa98744f29dd382ada195bc887de8eea
joonsub <joonsubtalk@gmail.com>
2017-01-11 13:22:42 -0800

    initial data fetch

---
+++ public/js/main.js
@@ -0,0 +1,34 @@
+// module
+var honeybookApp = angular.module('honeybookApp',['ngRoute','ngResource']);
+
+// routes
+honeybookApp.config(function($routeProvider){
+  $routeProvider
+  .when('/', {
+    templateUrl: 'pages/home.html',
+    controller: 'homeController'
+  })
+});
+
+// services
+honeybookApp.service('contactListService', function(){
+  this.contactList = '';
+});
+
+honeybookApp.factory('mainInfo', function($http) {
+    var getRecordsPromise = $http.get('https://candidate-test.herokuapp.com/contacts')
+      .then(function(response){
+        return response.data;
+    });
+
+    return {
+        getRecords: getRecordsPromise
+    };
+});
+
+
+honeybookApp.controller('homeController', function($scope, mainInfo){
+    mainInfo.getRecords.then(function (records) {
+        $scope.awesome = records;
+    });
+});
